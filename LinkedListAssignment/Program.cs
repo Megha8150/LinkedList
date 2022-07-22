e524ff927c7971b9c66976d1a41853ed82305d18
Megha8150 <meghananayak436@gmail.com>
2022-07-22 15:45:57 +0530

    Created Simple linked list

--- LinkedListAssignment/Program.cs
+++ LinkedListAssignment/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LinkedListAssignment;
 Console.WriteLine("Welcome to DataStructure Linkedlist!");
+
+LinkedList list = new LinkedList();
+list.Add(30);
+list.Add(56);
+list.Add(70);
+Console.Write("\nLinked List : ");
+list.Display();
