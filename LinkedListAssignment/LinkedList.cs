a7074081a25e1d5f342bfde9f8bf0e5c3ece7a4f
Megha8150 <meghananayak436@gmail.com>
2022-07-25 14:54:46 +0530

    Inserted 30 in between 56 and 70

--- LinkedListAssignment/LinkedList.cs
+++ LinkedListAssignment/LinkedList.cs
@@ -6,41 +6,58 @@ using System.Threading.Tasks;
 
 namespace LinkedListAssignment
 {
-    
-    
-        public class LinkedList
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
         {
-            public Node head;
-
-            public void Add(int data)
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
             {
-                Node node = new Node(data);
-                if (this.head == null)
-                    this.head = node;
-                else
-                {
-                    Node temp = head;
-                    while (temp.next != null)
-                        temp = temp.next;
-                    temp.next = node;
-                }
-                Console.WriteLine("{0} inserted into the linked list", node.data);
-            }
-            public void Display()
-            {
-                Node temp = this.head;
-                if (temp == null)
-                {
-                    Console.WriteLine("Linked list is empty");
-                    return;
-                }
-                while (temp != null)
-                {
-                    Console.Write(temp.data + "->");
+                Node temp = head;
+                while (temp.next != null)
                     temp = temp.next;
-                }
-
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next;
             }
         }
+        internal Node InsertAtParticularPosition(int PreviousData, Node Newdata)
+        {
+            Node temp = this.head;
+            while (temp != null) 
+            {
+                while (temp.data == PreviousData)
+                {
+                    Node newnode = temp.next;
+                    temp.next = Newdata;
+                    temp.next.next = newnode;
+
+                    break;
+                }
+                temp = temp.next;
+            }
+            return head;
+        }
     }
+}
+
+
+
+
 
--- LinkedListAssignment/Program.cs
+++ LinkedListAssignment/Program.cs
@@ -2,9 +2,12 @@
 using LinkedListAssignment;
 Console.WriteLine("Welcome to DataStructure Linkedlist!");
 
+
 LinkedList list = new LinkedList();
+Node node = new Node(30);
 list.Add(56);
-list.Add(30);
 list.Add(70);
-Console.Write("\nLinked List : ");
+list.Display();
+list.InsertAtParticularPosition(56, node);
+Console.Write("\n After Inserted 30 in between 56 and 70 :\n Linked List : ");
 list.Display();
